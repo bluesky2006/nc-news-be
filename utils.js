ffa8c4615790b05f47f57ba7ef37319d4410111c
bluesky2006 <simon.busby@gmail.com>
2025-06-06 16:27:56 +0100

    Update: disable checkTopicExists - needs more testing

--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
-const db = require("./db/connection");
+// const db = require("./db/connection");
 
-exports.checkTopicExists = (topic) => {
-  return db
-    .query(`SELECT * FROM topics WHERE slug = $1;`, [topic])
-    .then(({ rowCount }) => {
-      if (rowCount === 0) {
-        return false;
-      }
-      return true;
-    });
-};
+// exports.checkTopicExists = (topic) => {
+//   return db
+//     .query(`SELECT * FROM topics WHERE slug = $1;`, [topic])
+//     .then(({ rowCount }) => {
+//       if (rowCount === 0) {
+//         return false;
+//       }
+//       return true;
+//     });
+// };
